c91aa7ef455bd07bb62b9d67622f984d12f7f5f2
1811342 <n.p.abeykoonmudiyanselage@wlv.ac.uk>
2021-10-28 20:02:24 +0100

    inni setup

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import tensorflow as tf
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH =800;
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Pathfinding Algorithm")
